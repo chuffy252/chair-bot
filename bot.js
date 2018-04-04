8c96c6f5f8fdac60d7c80882e53b236d61286e2d
Justin Chu <justin.chu@rogers.com>
2018-04-03 23:35:10 -0400

    Bot files

---
+++ bot.js
@@ -0,0 +1,33 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+var msgs = 0;
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    if (message.user == 'Chuffy') {
+        msgs++;
+    }
+    if(msgs = 5)
+    {
+        msgs = 0;
+        bot.sendMessage({
+            to: channelID,
+            message: 'fuck you michael'
+        })
+    }
+});
