f317a725bc32e451307962cb2b6ad5f1f177918c
justin-chu <30902118+justin-chu@users.noreply.github.com>
2018-04-04 22:29:25 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var nospam = 0;
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-const noSpam = 0;
+
 
 client.on('message', message => {
     if (message.author.username === 'Chuffy') {
-        noSpam;
+        noSpam += 1;
     }
     if(noSpam == 5){
         message.reply('i love u chu');
         noSpam = 0;
     }
 });
-
-client.login(process.env.BOT_TOKEN);
