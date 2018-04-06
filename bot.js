9e1e2d2bce5a7da006cc433b72d37f6bd7e019ea
justin-chu <30902118+justin-chu@users.noreply.github.com>
2018-04-06 10:57:20 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,8 @@ client.on('message', message => {
             message.reply('i love u chu');
             nospam = 0;
         }
-    } else if (message.content.substring(0,2) === 'dog'){
+    } 
+    if (message.content.substring(0,3) === 'dog'){
         message.react('🐕');
     }
 });
