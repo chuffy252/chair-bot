0a2609605630b87e5d5b647fec8d34105428d603
justin-chu <30902118+justin-chu@users.noreply.github.com>
2018-04-11 10:53:11 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var nospam = 0;
 
 client.on('ready', () => {
     console.log('I am ready!');
-});
-
-
-
-client.on('message', message => {
-    if (message.author.username === 'Chuffy') {
-        nospam = nospam + 1;
-        if(nospam == 5){
-            message.reply('i love u chu');
-            nospam = 0;
-        }
-    } 
 });
 
 client.on('message', message => {
     if (message.content.substring(0,3) === 'dog'){
         message.react('🐕');
     }
+    if(message.content.substring(0, 17) === 'i love you chubot') {
+        if(message.author.username === 'Chuffy') {
+            message.reply('i love u chu');
+        }
+        else {
+            message.reply('shut the fuck up');
+        }
+    }
 });
+
 var http = require("http");
 setInterval(function() {
     http.get("https://chuffy-bot.herokuapp.com/");
