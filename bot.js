1483526cc632ea4b995a446c325aa9a7bb0ab947
justin-chu <30902118+justin-chu@users.noreply.github.com>
2018-04-12 14:29:43 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,9 +22,9 @@ client.on('message', message => {
     }
 });
 
-var http = require("http");
-setInterval(function() {
-    http.get("https://chuffy-bot.herokuapp.com/");
-}, 300000);
+
+
+
+
 
 client.login(process.env.BOT_TOKEN);
