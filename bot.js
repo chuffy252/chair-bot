aafe796b53e1b01e73c9d8e4be42202b641cbfec
justin-chu <30902118+justin-chu@users.noreply.github.com>
2019-09-11 22:39:31 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,6 +13,20 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
+    if (message.content.toLowerCase().substring(0,8) === ':pepega:'){
+        message.reply('is now on the official CSIS watchlist for being a suspected terrorist threat');
+    }
+});
+
+client.on('message', message => {
+    if (message.content.toLowerCase().substring(0,5) === 'hello'){
+        message.reply('is currently commiting hate crimes against minorities');
+    }
+});
+
+
+
+client.on('message', message => {
     if(message.content.toLowerCase().substring(0, 17) === 'i hate you chubot'){
         message.reply('i will literally tear your throat out i swear i dare you to come at me come on you little shit dont act tough online and expect no repercussions huh what you waiting for yeah thats what i thought you absolute loser haha. pathetic.');
     }
